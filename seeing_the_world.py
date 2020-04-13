7eea3fb7a55199d819e77421f87149316a2f7f48
gkbiring <62904855+gkbiring@users.noreply.github.com>
2020-04-13 16:36:41 -0700

    Create seeing_the_world.py

---
+++ seeing_the_world.py
@@ -0,0 +1,31 @@
+destinations = ['italy', 'spain', 'greece', 'banff', 'maldives']
+print(destinations)
+
+#sorted list
+print("\nHere is the sorted list:")
+print(sorted(destinations))
+
+#original order
+print(destinations)
+
+#reverse alphabetical order
+print(sorted(destinations, reverse=True))
+
+#original order
+print(destinations)
+
+#reverse order
+destinations.reverse()
+print(destinations)
+
+#reverse again
+destinations.reverse()
+print(destinations)
+
+#alphabetical order
+destinations.sort()
+print(destinations)
+
+#reverse alphabetical order
+destinations.sort(reverse=True)
+print(destinations)
