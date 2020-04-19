9ce752ec80e684b36cf1996b416af069939c6af6
gkbiring <62904855+gkbiring@users.noreply.github.com>
2020-04-18 18:30:25 -0700

    Create dog_loop.py

---
+++ dog_loop.py
@@ -0,0 +1,8 @@
+dogs = ['shiba inu', 'husky', 'samoyed']
+for dog in dogs:
+    print(dog)
+#Sentence about dog
+print("\n")
+for dog in dogs:
+    print(f"A {dog.title()} would make a great pet.")
+print("\nAny of these dogs would make a great pet!")
