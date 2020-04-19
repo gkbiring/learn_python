780463ac9c94a8dcfe32db01f5547f3cdd625f82
gkbiring <62904855+gkbiring@users.noreply.github.com>
2020-04-18 18:24:47 -0700

    Create pizza_loop.py

---
+++ pizza_loop.py
@@ -0,0 +1,8 @@
+favorite_pizzas = ['cheese', 'pepperoni', 'olive']
+for pizza in favorite_pizzas:
+    print(pizza)
+print("\n")
+#Print favorite pizza sentence
+for pizza in favorite_pizzas:
+    print(f"I really love {pizza} pizza.")
+print("\nI really love pizza!")
