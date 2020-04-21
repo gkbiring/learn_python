6f23c3afb539744a4657709d6101decea09da42e
gkbiring <62904855+gkbiring@users.noreply.github.com>
2020-04-20 17:36:58 -0700

    Create slices.py

---
+++ slices.py
@@ -0,0 +1,10 @@
+favorite_pizzas = ['cheese', 'pepperoni', 'olive', 'combination', 'veggie']
+#First three items in the list
+print("The first three items in the list are:")
+print(favorite_pizzas[0:3])
+#Middle three items in the list
+print("\nThree items from the middle of the list are:")
+print(favorite_pizzas[1:4])
+#Last three items in the list
+print("\nThe last three items in the list are:")
+print(favorite_pizzas[-3:])
