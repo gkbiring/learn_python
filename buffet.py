f78142f58dc58c8b2f520c39db3b4064ff9388d1
gkbiring <62904855+gkbiring@users.noreply.github.com>
2020-04-20 18:22:01 -0700

    Create buffet.py

---
+++ buffet.py
@@ -0,0 +1,11 @@
+#Tuple with for loop
+menu_items = ('fries', 'salad', 'chicken', 'steak', 'gravey')
+print("We offer the following items on our menu:")
+for menu_item in menu_items:
+    print("- " + menu_item)
+
+
+menu_items = ('mashed potatoes', 'salad', 'chicken', 'fish', 'gravey')
+print("\nOur menu has now changed to the following:")
+for menu_item in menu_items:
+    print("- " + menu_item)
