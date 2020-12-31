88b1175c8bbcc71427a03ff39c2fbfe0574355df
gkbiring <62904855+gkbiring@users.noreply.github.com>
2020-12-31 15:29:09 -0800

    Create persons.py

---
+++ persons.py
@@ -0,0 +1,5 @@
+person = {'first_name': 'pervinder', 'last_name': 'sandhu', 'age': '33', 'city': 'fremont'}
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
