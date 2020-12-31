c017a2e9d1e5c4e396b48bdbbb0f491678a49c52
gkbiring <62904855+gkbiring@users.noreply.github.com>
2020-12-31 15:39:31 -0800

    Create favorite_numbers.py

---
+++ favorite_numbers.py
@@ -0,0 +1,22 @@
+favorite_numbers = {
+    'amrinder': '5',
+    'guri': '7',
+    'ritta': '13',
+    'caleb': '8',
+    'dev': '18',
+}
+
+number = (favorite_numbers['amrinder'])
+print(f"Amrinder's favorite number is {number}.")
+
+number = (favorite_numbers['guri'])
+print(f"Guri's favorite number is {number}.")
+
+number = (favorite_numbers['ritta'])
+print(f"Ritta's favorite number is {number}.")
+
+number = (favorite_numbers['caleb'])
+print(f"Caleb's favorite number is {number}.")
+
+number = (favorite_numbers['dev'])
+print(f"Dev's favorite number is {number}.")
