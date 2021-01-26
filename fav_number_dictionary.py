50ea19709df6da2c688efaabbfde41c456063e18
gkbiring <62904855+gkbiring@users.noreply.github.com>
2021-01-25 21:56:26 -0800

    Create fav_number_dictionary.py

---
+++ fav_number_dictionary.py
@@ -0,0 +1,18 @@
+favorite_numbers = {
+    'caleb': '9',
+    'mandy': '12',
+    'ritta': '7',
+    'mindy': '5',
+    'marsha': '15',
+    }
+
+number = favorite_numbers['caleb']
+print(f"Caleb's favorite number is {number}.")
+number = favorite_numbers['mandy']
+print(f"Mandy's favorite number is {number}.")
+number = favorite_numbers['ritta']
+print(f"Ritta's favorite number is {number}.")
+number = favorite_numbers['mindy']
+print(f"Mindy's favorite number is {number}.")
+number = favorite_numbers['marsha']
+print(f"Marsha's favorite number is {number}.")
