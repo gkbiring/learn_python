8798cc9433ba15aea61cf41d1f682313d8ae7d1d
gkbiring <62904855+gkbiring@users.noreply.github.com>
2021-01-25 21:46:34 -0800

    Create person_dictionary.py

---
+++ person_dictionary.py
@@ -0,0 +1,11 @@
+person = {
+    'first_name': 'bob',
+    'last_name': 'builder',
+    'age': '30',
+    'city': 'sacramento',
+    }
+
+print(person['first_name'].title())
+print(person['last_name'].title())
+print(person['age'])
+print(person['city'].title())
